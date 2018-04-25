3f1944bb842aebeda4045284e0adbbf42f9f364f
maxwellspann@gmail.com <maxwellspann@gmail.com>
2018-04-25 12:10:21 -0700

    Working build

--- hometext.py
+++ hometext.py
@@ -9,15 +9,15 @@ Created on Tue Apr 24 18:45:06 2018
 import sys
 
 filename = sys.argv[1]
-hostnum = sys.argv[2]
+hostnum = int(sys.argv[2])
 
 file = open(filename, "r")
 for line in file:
-   print line,
-
-f= open("sh-172.23.1.%s" % hostnum, "w+")
-for i in range(10):
-     f.write("This is line %d\r\n" % (i+1))
-f.close()
+   x = line.rstrip()
+   f = open("sh-172.23.1.%s" % hostnum, "w+")
+   f.write("host sh-172-23-1-%s { \n hardware ethernet %s; \n fixed-address 172.23.1.%s;\n }"  % ( hostnum, x, hostnum ))
+   f.close()
+   hostnum = hostnum + 1
+  
 
  
